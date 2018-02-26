105a1cc2723d915473adffc6c1c5f0c319254d79
BetterEdition <jesperenemark@hotmail.com>
2018-02-26 13:49:03 +0100

    initial commit

---
+++ app/src/main/java/com/example/jesperenemark/meyerapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jesperenemark.meyerapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
