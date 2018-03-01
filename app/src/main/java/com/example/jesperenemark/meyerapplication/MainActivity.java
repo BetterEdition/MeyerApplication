7e335fa9dfedcc5fccb91f7385833b380c7c57c5
Edwin Silva <edwinsilva@Edwins-MacBook-Pro.local>
2018-03-01 12:55:02 +0100

    emdel

--- app/src/main/java/com/example/jesperenemark/meyerapplication/MainActivity.java
+++ app/src/main/java/com/example/jesperenemark/meyerapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jesperenemark.meyerapplication;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,20 +13,17 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import java.util.ArrayList;
-
+import java.util.stream.*;
+import org.w3c.dom.Text;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private static final String KEYINDEX_1 = "index1";
-    private static final String KEYINDEX_2 = "index2";
+    private static final String KEY_Index  = "index";
 
-    int  dice_1 = 0;
-    int  dice_2 = 0;
-
-
+    public String dicesString;
     private Button mRollButton;
     private Button m2ndActivityButton;
     private TextView txtResult;
@@ -38,8 +36,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG , "onSavedInstanceState");
+        Log.d(TAG, "onCreate: called");
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null){
+            dicesString = savedInstanceState.getString(KEY_Index, null);
+        }
 
         this.numbersOfDie = new ArrayList<>();
         mSpinner = (Spinner) findViewById(R.id.spinner);
@@ -47,21 +48,6 @@ public class MainActivity extends AppCompatActivity {
         m2ndActivityButton = (Button) findViewById(R.id.btnHistory);
         txtResult = (TextView) findViewById(R.id.txtResult);
         populateSpinner();
-
-        referenceViewObjects();
-        if (savedInstanceState !=null){
-            dice_1 = savedInstanceState.getInt(KEYINDEX_1, 0);
-            dice_2 = savedInstanceState.getInt(KEYINDEX_2, 0);
-
-        }
-        else
-
-
-
-        numberOfDie();
-
-
-
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
@@ -69,13 +55,59 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("CheckOnSelect", "onItemSelected: "+numberChosen);
             }
 
+
+
+
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
                 numberChosen = "1";
             }
         });
+
+
+
+
         rollDie();
 
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart: called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: called");
+        txtResult.setText(dicesString)
+
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: called");
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState)
+    {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG,"onSaveInstanceState");
+        savedInstanceState.putString(KEY_Index,dicesString);
+
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop: called");
+    }
+    public  void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: called");
+    }
+
+
+    public void onNothingSelected(AdapterView<?> parentView) {
+        numberChosen = "1";
     }
 
 
@@ -88,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void rollDie(){
+
         mRollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -116,91 +149,14 @@ public class MainActivity extends AppCompatActivity {
                 for (i=0; i < numbersOfDie.size(); i++) {
                     result += numbersOfDie.get(i);
                 }
-                txtResult.setText(txtResult.getText().toString() + result);
+                dicesString = (txtResult.getText().toString() + result);
+                txtResult.setText(dicesString);
 
 
-
-
-                diceRandomizer();
 
 
             }
         });
-
-    }
-
-    @Override
-    public void onStart(){
-        super.onStart();
-        Log.d(TAG, "onStart: called");
-
-;
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume: called");
-        mDie1.setText(String.valueOf(dice_1));
-        mDie2.setText(String.valueOf(dice_2));
-        mResult.setText(String.valueOf(dice_1 + dice_2));
-
-
-
-    }
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause: called");
-    }
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState){
-        super.onSaveInstanceState(savedInstanceState);
-        Log.i(TAG, "onSavedInstanceState: ");
-        savedInstanceState.putInt(KEYINDEX_2,dice_2);
-        savedInstanceState.putInt(KEYINDEX_1,dice_1);
-    }
-
-
-    @Override
-    public void onStop(){
-        super.onStop();
-        Log.d(TAG, "onStop: called");
-    }
-    public  void onDestroy(){
-        super.onDestroy();
-        Log.d(TAG, "onDestroy: called");
-    }
-
-
-    public void referenceViewObjects(){
-        setContentView(R.layout.activity_main);
-        mSpinner = (Spinner) findViewById(R.id.spinner);
-        mDie1 = (TextView) findViewById(R.id.txtdice1);
-        mDie2 = (TextView) findViewById(R.id.txtdice2);
-        mResult = (TextView) findViewById(R.id.txtResult);
-        mRollButton = (Button) findViewById(R.id.btnRoll);
-        m2ndActivityButton = (Button) findViewById(R.id.btnHistory);
-    }
->
-    public void numberOfDie() {
-        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(this,   android.R.layout.simple_spinner_item, numbers);
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpinner.setAdapter(spinnerArrayAdapter);
-
-    }
-
-
-        public void diceRandomizer(){
-            Log.i(TAG, "diceRandomizer:was called");
-        Random rand = new Random();
-        dice_1 = rand.nextInt(6) + 1;
-        dice_2 = rand.nextInt(6) + 1;
-        mDie1.setText(String.valueOf(dice_1));
-        mDie2.setText(String.valueOf(dice_2));
-
     }
 
 }
-
-
-
